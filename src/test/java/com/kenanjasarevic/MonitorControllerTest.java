3bc6cd90c814e77464a444b4037583b756c6ed2c
Kenan Jasarevic <wi20b106@technikum-wien.at>
2021-06-26 19:12:31 +0200

    Removing unnecessary test

--- src/test/java/com/kenanjasarevic/MonitorControllerTest.java
+++ src/test/java/com/kenanjasarevic/MonitorControllerTest.java
@@ -5,15 +5,6 @@ import org.junit.jupiter.api.Test;
 import javax.ws.rs.core.Response;
 
 public class MonitorControllerTest {
-    @Test // Method name must start with "test"
-    public void test_calculateUptime_shouldBeCalculatedCorrectly() {
-        // Arrange
-        MonitorController controller = new MonitorController();
-        // Act
-        String actValue = controller.calculateUptime(99.95);
-        // Assert
-        Assertions.assertEquals("43178.40", actValue);
-    }
 
     @Test
     public void test_getResponse_shouldGetResponse() {
